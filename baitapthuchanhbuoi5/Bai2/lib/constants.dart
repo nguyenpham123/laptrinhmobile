292dca4c5dafa387cacb49ade006ffc86c454a49
nguyenpham123 <139855249+nguyenpham123@users.noreply.github.com>
2023-08-17 15:02:52 +0700

    Baitap

---
+++ baitapthuchanhbuoi5/Bai2/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyBsY6qXIExaN7I6hOCHu-RvaQ0S2awFGoU";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
